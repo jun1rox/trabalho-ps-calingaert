61fc8ba5b7b64d49a22e87936661776262716704
franconoronha <franconoronha11@gmail.com>
2022-09-07 17:32:43 -0300

    registradores

--- Calingaert/src/calingaert/Calingaert.java
+++ Calingaert/src/calingaert/Calingaert.java
@@ -1,15 +1,11 @@
 package calingaert;
 
-import java.awt.BorderLayout;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JFrame;
-import trabalho_ps.FuncoesUteis;
-import javax.swing.JTextPane;
 import trabalho_ps.Memoria;
 import trabalho_ps.Registrador;
+import trabalho_ps.FuncoesUteis;
 
 /**
  *
@@ -21,28 +17,22 @@ public class Calingaert {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ArrayList<Registrador> registradores = new ArrayList<>();
-        Memoria memoria = new Memoria(registradores);
+
+        Memoria memoria = new Memoria();
+        String valor_pc = FuncoesUteis.intToBinaryString(memoria.getINICIO_INS_DADOS(), 16);
+        Registrador PC = new Registrador(valor_pc);
+        Registrador ACC = new Registrador();
+        Registrador SP = new Registrador();
+        Registrador MOP = new Registrador();
+        Registrador RI = new Registrador();
+        Registrador RE = new Registrador();
         String caminho = "../programa.txt";
         try {
             memoria.carregaPrograma(caminho);
         } catch (IOException ex) {
             Logger.getLogger(Calingaert.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
-        JFrame frame = new JFrame("Calingaert");
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        frame.setVisible(true);
-        JTextPane painel = new JTextPane();
-        frame.getContentPane().add(BorderLayout.CENTER, painel);
-        
-        try {
-            FuncoesUteis.imprimeInterfaceFinal(painel, caminho, memoria.getMemoria());
-            // TODO code application logic here
-        } catch (IOException ex) {
-            Logger.getLogger(Calingaert.class.getName()).log(Level.SEVERE, null, ex);
-        }
+
     }
 
 }
