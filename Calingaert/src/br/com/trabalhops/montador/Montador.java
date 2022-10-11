612f89a4399dcda8a290de13ea1307282ffc04c2
ElizabethAyia <79996413+ElizabethAyia@users.noreply.github.com>
2022-10-10 22:00:50 -0300

    Inicio do tratamento de erros estruturais

--- Calingaert/src/br/com/trabalhops/montador/Montador.java
+++ Calingaert/src/br/com/trabalhops/montador/Montador.java
@@ -24,20 +24,55 @@ public class Montador {
     public boolean monta() throws IOException {
         BufferedReader buffRead = new BufferedReader(new FileReader(caminho));
         String linha = buffRead.readLine();
-        int i = 0;
+        int posInstrucao;
+        String[] palavras;
         
         ArrayList<Simbolo> simbolos = new ArrayList();
         
         while (linha != null) {
+            linha = linha.split("\\*")[0];
+            
             if (linha.length() > 80) {
                 System.out.println("Ta muito grande, bb");
             } else {
                 linha = linha.replaceAll("\\s+", " ");
                 linha = linha.trim();
-                for (String s : linha.split(" ")) {
-                    System.out.print(s);
-                    System.out.println(instrucoes.checkInstrucao(s));
-                    
+                palavras = linha.split(" ");
+                
+                if (instrucoes.checkInstrucao(palavras[0])) // se ele entrar nesse if é pq a primeira palavra é a instrucao
+                {
+                    posInstrucao = 0; 
+                }
+                else
+                {
+                    if (palavras.length < 2) // testa se existe segunda palavra na linha
+                    {
+                        // ERROR, TOO FEW ARGUMENTS (SO TEM UMA LABEL NA LINHA)
+                        // RETURN
+                    }
+                    if (!instrucoes.checkInstrucao(palavras[1]))
+                    {
+                        // ERROR, THERE'S NO INSTRUCTION IN THE LINE
+                        // RETURN
+                    }
+
+                    posInstrucao = 1;
+                }
+                // adendo, tem que fazer mais testes nos operandos
+                int instrucao_e_opcodes[] = instrucoes.getInstrucao(palavras[posInstrucao]);
+                int operandoAmount = palavras.length - instrucao_e_opcodes[1];
+
+                if(operandoAmount > posInstrucao + 1)
+                {
+                    // ERROR, TOO MANY ARGUMENTS
+                }
+                else if (operandoAmount < posInstrucao + 1)
+                {
+                    // ERROR, TOO FEW ARGUMENTS
+                }
+                else
+                {
+                    // tudo certo na parte de operandos
                 }
             }
             linha = buffRead.readLine();
